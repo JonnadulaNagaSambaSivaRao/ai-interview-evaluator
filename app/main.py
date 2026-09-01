f0e3a4b5ec6c98446d0ccb47460ad164fff032d1
Sambasivarao <sivajonnadula99@gmail.com>
2026-09-01 22:02:31 +0530

    first commit

--- app/main.py
+++ app/main.py
@@ -1,51 +1,33 @@
 from fastapi import FastAPI
-
-from app.database import Base, engine
-
-# Import all models
-from app.models import (
-    User,
-    JobRole,
-    Interview,
-    InterviewQuestion,
-    InterviewEligibility,
-    InterviewAttempt,
-    Evaluation,
-    FinalReport
-)
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.auth import router as auth_router
-from app.routers.admin import router as admin_router
-from app.routers.candidate import router as candidate_router
-
-
-Base.metadata.create_all(
-    bind=engine
-)
-
 
 app = FastAPI(
     title="AI Interview Evaluator API",
-    version="1.0.0"
 )
 
+# --------------------------------------------------
+# CORS
+# --------------------------------------------------
 
-app.include_router(
-    auth_router
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-app.include_router(
-    admin_router
-)
+# --------------------------------------------------
+# Routers
+# --------------------------------------------------
 
-app.include_router(
-    candidate_router
-)
+app.include_router(auth_router)
 
 
 @app.get("/")
 def root():
-
     return {
         "message": "AI Interview Evaluator API is running"
     }
